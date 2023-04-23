5a47e7922ff417743f92188d03cc57ff91c810fc
Olca Orakcı <olcaorakci@gmail.com>
2023-04-23 23:22:04 +0300

    added luxai and weekly tabular competition

--- luxai/agent.py
+++ luxai/agent.py
@@ -1,88 +1,116 @@
-from lux.kit import obs_to_game_state, GameState, EnvConfig
-from lux.utils import direction_to, my_turn_to_place_factory
-import numpy as np
+"""
+This file is where your agent's logic is kept. Define a bidding policy, factory placement policy, as well as a policy for playing the normal phase of the game
+
+The tutorial will learn an RL agent to play the normal phase and use heuristics for the other two phases.
+
+Note that like the other kits, you can only debug print to standard error e.g. print("message", file=sys.stderr)
+"""
+
+import os.path as osp
 import sys
-class Agent():
+import numpy as np
+import torch as th
+from stable_baselines3.ppo import PPO
+from lux.config import EnvConfig
+from wrappers import SimpleUnitDiscreteController, SimpleUnitObservationWrapper
+from controller import UnitDiscreteController
+
+# change this to use weights stored elsewhere
+# make sure the model weights are submitted with the other code files
+# any files in the logs folder are not necessary. Make sure to exclude the .zip extension here
+MODEL_WEIGHTS_RELATIVE_PATH = "./best_model"
+
+class Agent:
     def __init__(self, player: str, env_cfg: EnvConfig) -> None:
         self.player = player
         self.opp_player = "player_1" if self.player == "player_0" else "player_0"
         np.random.seed(0)
         self.env_cfg: EnvConfig = env_cfg
 
-    def early_setup(self, step: int, obs, remainingOverageTime: int = 60):
-        if step == 0:
-            # bid 0 to not waste resources bidding and declare as the default faction
-            return dict(faction="AlphaStrike", bid=0)
-        else:
-            game_state = obs_to_game_state(step, self.env_cfg, obs)
-            # factory placement period
+        directory = osp.dirname(__file__)
+        self.policy = PPO.load(osp.join(directory, MODEL_WEIGHTS_RELATIVE_PATH))
 
-            # how much water and metal you have in your starting pool to give to new factories
-            water_left = game_state.teams[self.player].water
-            metal_left = game_state.teams[self.player].metal
+        self.controller = UnitDiscreteController(self.env_cfg)
 
-            # how many factories you have left to place
-            factories_to_place = game_state.teams[self.player].factories_to_place
-            # whether it is your turn to place a factory
-            my_turn_to_place = my_turn_to_place_factory(game_state.teams[self.player].place_first, step)
-            if factories_to_place > 0 and my_turn_to_place:
-                # we will spawn our factory in a random location with 150 metal and water if it is our turn to place
-                potential_spawns = np.array(list(zip(*np.where(obs["board"]["valid_spawns_mask"] == 1))))
-                spawn_loc = potential_spawns[np.random.randint(0, len(potential_spawns))]
-                return dict(spawn=spawn_loc, metal=150, water=150)
+    def bid_policy(self, step: int, obs, remainingOverageTime: int = 60):
+        # the policy here is the same one used in the RL tutorial: https://www.kaggle.com/code/stonet2000/rl-with-lux-2-rl-problem-solving
+        return dict(faction="AlphaStrike", bid=0)
+
+    def factory_placement_policy(self, step: int, obs, remainingOverageTime: int = 60):
+        # the policy here is the same one used in the RL tutorial: https://www.kaggle.com/code/stonet2000/rl-with-lux-2-rl-problem-solving
+        if obs["teams"][self.player]["metal"] == 0:
             return dict()
+        potential_spawns = list(zip(*np.where(obs["board"]["valid_spawns_mask"] == 1)))
+        potential_spawns_set = set(potential_spawns)
+        done_search = False
+
+        ice_diff = np.diff(obs["board"]["ice"])
+        pot_ice_spots = np.argwhere(ice_diff == 1)
+        if len(pot_ice_spots) == 0:
+            pot_ice_spots = potential_spawns
+        trials = 5
+        while trials > 0:
+            pos_idx = np.random.randint(0, len(pot_ice_spots))
+            pos = pot_ice_spots[pos_idx]
+
+            area = 3
+            for x in range(area):
+                for y in range(area):
+                    check_pos = [pos[0] + x - area // 2, pos[1] + y - area // 2]
+                    if tuple(check_pos) in potential_spawns_set:
+                        done_search = True
+                        pos = check_pos
+                        break
+                if done_search:
+                    break
+            if done_search:
+                break
+            trials -= 1
+        spawn_loc = potential_spawns[np.random.randint(0, len(potential_spawns))]
+        if not done_search:
+            pos = spawn_loc
+
+        metal = obs["teams"][self.player]["metal"]
+        return dict(spawn=pos, metal=metal, water=metal)
 
     def act(self, step: int, obs, remainingOverageTime: int = 60):
-        actions = dict()
-        game_state = obs_to_game_state(step, self.env_cfg, obs)
-        factories = game_state.factories[self.player]
-        game_state.teams[self.player].place_first
-        factory_tiles, factory_units = [], []
-        for unit_id, factory in factories.items():
-            if factory.power >= self.env_cfg.ROBOTS["HEAVY"].POWER_COST and \
-            factory.cargo.metal >= self.env_cfg.ROBOTS["HEAVY"].METAL_COST:
-                actions[unit_id] = factory.build_heavy()
-            if self.env_cfg.max_episode_length - game_state.real_env_steps < 50:
-                if factory.water_cost(game_state) <= factory.cargo.water:
-                    actions[unit_id] = factory.water()
-            factory_tiles += [factory.pos]
-            factory_units += [factory]
-        factory_tiles = np.array(factory_tiles)
+        # first convert observations using the same observation wrapper you used for training
+        # note that SimpleUnitObservationWrapper takes input as the full observation for both players and returns an obs for players
+        raw_obs = dict(player_0=obs, player_1=obs)
+        obs = SimpleUnitObservationWrapper.convert_obs(raw_obs, env_cfg=self.env_cfg)
+        obs = obs[self.player]
 
-        units = game_state.units[self.player]
-        ice_map = game_state.board.ice
-        ice_tile_locations = np.argwhere(ice_map == 1)
-        for unit_id, unit in units.items():
+        obs = th.from_numpy(obs).float()
+        with th.no_grad():
 
-            # track the closest factory
-            closest_factory = None
-            adjacent_to_factory = False
-            if len(factory_tiles) > 0:
-                factory_distances = np.mean((factory_tiles - unit.pos) ** 2, 1)
-                closest_factory_tile = factory_tiles[np.argmin(factory_distances)]
-                closest_factory = factory_units[np.argmin(factory_distances)]
-                adjacent_to_factory = np.mean((closest_factory_tile - unit.pos) ** 2) == 0
+            # to improve performance, we have a rule based action mask generator for the controller used
+            # which will force the agent to generate actions that are valid only.
+            action_mask = (
+                th.from_numpy(self.controller.action_masks(self.player, raw_obs))
+                .unsqueeze(0)
+                .bool()
+            )
 
-                # previous ice mining code
-                if unit.cargo.ice < 40:
-                    ice_tile_distances = np.mean((ice_tile_locations - unit.pos) ** 2, 1)
-                    closest_ice_tile = ice_tile_locations[np.argmin(ice_tile_distances)]
-                    if np.all(closest_ice_tile == unit.pos):
-                        if unit.power >= unit.dig_cost(game_state) + unit.action_queue_cost(game_state):
-                            actions[unit_id] = [unit.dig(repeat=0)]
-                    else:
-                        direction = direction_to(unit.pos, closest_ice_tile)
-                        move_cost = unit.move_cost(game_state, direction)
-                        if move_cost is not None and unit.power >= move_cost + unit.action_queue_cost(game_state):
-                            actions[unit_id] = [unit.move(direction, repeat=0)]
-                # else if we have enough ice, we go back to the factory and dump it.
-                elif unit.cargo.ice >= 40:
-                    direction = direction_to(unit.pos, closest_factory_tile)
-                    if adjacent_to_factory:
-                        if unit.power >= unit.action_queue_cost(game_state):
-                            actions[unit_id] = [unit.transfer(direction, 0, unit.cargo.ice, repeat=0)]
-                    else:
-                        move_cost = unit.move_cost(game_state, direction)
-                        if move_cost is not None and unit.power >= move_cost + unit.action_queue_cost(game_state):
-                            actions[unit_id] = [unit.move(direction, repeat=0)]
-        return actions
+            # SB3 doesn't support invalid action masking. So we do it ourselves here
+            features = self.policy.policy.features_extractor(obs.unsqueeze(0))
+            x = self.policy.policy.mlp_extractor.shared_net(features)
+            logits = self.policy.policy.action_net(x) # shape (1, N) where N=12 for the default controller
+
+            logits[~action_mask] = -1e8 # mask out invalid actions
+            dist = th.distributions.Categorical(logits=logits)
+            actions = dist.sample().cpu().numpy() # shape (1, 1)
+
+        # use our controller which we trained with in train.py to generate a Lux S2 compatible action
+        lux_action = self.controller.action_to_lux_action(
+            self.player, raw_obs, actions[0], step
+        )
+
+        # commented code below adds watering lichen which can easily improve your agent
+        # shared_obs = raw_obs[self.player]
+        # factories = shared_obs["factories"][self.player]
+        # for unit_id in factories.keys():
+        #     factory = factories[unit_id]
+        #     if 1000 - step < 50 and factory["cargo"]["water"] > 100:
+        #         lux_action[unit_id] = 2 # water and grow lichen at the very end of the game
+
+        return lux_action
